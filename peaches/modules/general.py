743a28beaa20d97b4679037500fa775a6cee92fd
trumully <truman.mulholland@gmail.com>
2020-03-01 14:25:41 +1300

    Fixed emojis

--- peaches/modules/general.py
+++ peaches/modules/general.py
@@ -19,17 +19,17 @@ class General(commands.Cog):
         uptime = datetime.timedelta(microseconds=(time.time_ns() - self.bot.start_time) / 1000)
         uptime = str(uptime).split(".")[0]
         embed = discord.Embed(
-            title=f"About: {self.bot.user.name} | ID: {self.bot.user.id}",
+            title=f"{self.bot.custom_emojis.bot} About: {self.bot.user.name} | ID: {self.bot.user.id}",
             description=f"{self.bot.description}\n"
                         f"Serving **{user_amount} users** on **{guild_amount} guilds**",
             color=self.bot.colors.neutral)
         embed.set_thumbnail(url=self.bot.user.avatar_url_as(static_format="png"))
         embed.add_field(name="Information", value=f"Owner: {self.bot.owner.mention}\nUptime: {uptime}")
-        embed.add_field(name="Versions", value=f"Python {platform.python_version()}\n"
-                                               f"Discord {discord.__version__}")
-        embed.add_field(name="Process", value=f"CPU{psutil.cpu_percent()}% / "
+        embed.add_field(name="Versions", value=f"{self.bot.custom_emojis.python} {platform.python_version()}\n"
+                                               f"{self.bot.custom_emojis.discord} {discord.__version__}")
+        embed.add_field(name="Process", value=f"{self.bot.custom_emojis.cpu} {psutil.cpu_percent()}% / "
                                               f"{round(psutil.cpu_freq().current, 2)}MHz\n"
-                                              f"RAM {psutil.virtual_memory()[2]}%")
+                                              f"{self.bot.custom_emojis.vram} {psutil.virtual_memory()[2]}%")
 
         await ctx.send(embed=embed)
 
